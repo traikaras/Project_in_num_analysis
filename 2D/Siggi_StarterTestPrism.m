5061ab72e94fbd6e4def4676608fb6556cdd9d6a
siggisigg <sig.ag.sig@gmail.com>
2019-07-03 16:21:02 +0200

    Still wobles but moving on

--- 2D/Siggi_StarterTestPrism.m
+++ 2D/Siggi_StarterTestPrism.m
@@ -2,9 +2,9 @@
 rho = 10;
 lambda = 1;
 mu = 1;
-f = [0;0.1];
+f = [0;0];
 h = 1;
-p1 = [0;1];
+p1 = [0;sqrt(5)];
 p2 = [1;0];
 p3 = [-1;0];
 
@@ -13,7 +13,7 @@ p3 = [-1;0];
 % u_3D = 0.5*(p1+p3);
 u_1D = [0;0];
 u_3D = [0;0];
-tau_23N =[0;-0.01];
+tau_23N =[0;-0.1];
 
 %% Setup
 Itwo = eye(2);
@@ -22,14 +22,14 @@ P = vertcat([p1 p2 p3],ones(1,3));
 Q = P^-1;
 partE = [1 0 0 0;0 0.5 0.5 0;0 0.5 0.5 0;0 0 0 0.5];
 E = partE*kron(Q(1:2,:),Itwo);
-volB = h*abs(det(P))/2;
-% T's
-l_12 = norm(p1-p2);
-l_23 = norm(p2-p3);
-l_31 = norm(p1-p3);
-T_12 = l_12*h/2*[Itwo;Itwo;ztwo];
-T_23 = l_23*h/2*[ztwo;Itwo;Itwo];
-T_31 = l_31*h/2*[Itwo;ztwo;Itwo];
+volB = h*abs(det(P))*0.5;
+% % T's
+% l_12 = norm(p1-p2);
+% l_23 = norm(p2-p3);
+% l_31 = norm(p1-p3);
+% T_12 = l_12*h/2*[Itwo;Itwo;ztwo];
+% T_23 = l_23*h*[ztwo;Itwo;Itwo]*0.5;
+% T_31 = l_31*h/2*[Itwo;ztwo;Itwo];
 
 V = volB/3*[Itwo;Itwo;Itwo];
 %% Mass and stiffness matrix 
@@ -40,76 +40,62 @@ S = E'*lambda_mu*E;
 %% Extended system
 % Mass matrix
 M_extend = zeros(10);
-M_extend(1:6,1:6)=M;
+M_extend(1:6,1:6) = M;
 % Stiffness matrix
 C = 0.5*[Itwo Itwo;Itwo ztwo;ztwo Itwo];
 S_extend = zeros(10);
 S_extend(1:6,:)=[S C];
 S_extend(7:10,1:6)=C';
 % Right hand side
-rhs = vertcat(T_23*tau_23N+V*f,u_1D,u_3D);
+rhs = vertcat([ztwo;Itwo;Itwo]*0.5*tau_23N+V*f,u_1D,u_3D);
 
 %% Calculating the time evolution
 T = 20;
 nt = 200;
 dt = T/nt;
 p = zeros(6,nt+1); % Set of all points
-p(:,1) = vertcat(p1,p2,p3);
-U = zeros(10,nt+1);
-Up = zeros(10,1);
-Upp = zeros(10,1);
-%% Starting from a Stationary solution
-%U = S_extend\rhs;
-% NOT WORKING 
-%% Solving everything
 
+U = zeros(10,nt+1);
+Up = zeros(10,1); % Could be wrong 
+Upp = zeros(10,1); % Could be wrong
+%% Starting from a Stationary solution
+% U(:,1) = S_extend\rhs;
+
+% fig = figure;
+% axis([-1.25 1.25 -0.25 1.25])
+% patch()
+% p1 = p1+U(1:2,1);
+% p2 = p2+U(3:4,1);
+% p3 = p3+U(5:6,1);
+%NOT WORKING 
+%% Solving everything
+p(:,1) = vertcat(p1,p2,p3);
 for i=1:nt
     % Solve using the Newark method
     [U(:,i+1),Up,Upp] = Newark1step( M_extend, S_extend, rhs, U(:,i), Up ,Upp,dt);
-    % Define the new position of the points
+    % Define the new position of the points (now p1, p2, p3)
     p1 = U(1:2,i+1)+p1;
     p2 = U(3:4,i+1)+p2;
     p3 = U(5:6,i+1)+p3;
-    % Now update the variables dependend on them
-    l_12 = norm(p1-p2);
-    l_23 = norm(p2-p3);
-    l_31 = norm(p1-p3);
-    T_12 = l_12*h/2*[Itwo;Itwo;ztwo];
-    T_23 = l_23*h/2*[ztwo;Itwo;Itwo];
-    T_31 = l_31*h/2*[Itwo;ztwo;Itwo];
-    
+    % Store the new positions in a new column of matrix p
     p(:,i+1) = vertcat(p1,p2,p3);
-    P = vertcat([p1 p2 p3],ones(1,3));
-    volB = h*abs(det(P))/2;
-    Q = P^-1;
-    E = partE*kron(Q(1:2,:),Itwo);
-    V = volB/3*[Itwo;Itwo;Itwo];
-    % Update mass and stiffness matrices
-    M = ones(6)+eye(6)*rho*volB/12;
-    M_extend(1:6,1:6)=M;
-    S = E'*lambda_mu*E;
-    S_extend(1:6,:)=[S C];
-    S_extend(7:10,1:6)=C';
-    rhs = vertcat(T_23*tau_23N+V*f,u_1D,u_3D);
     
     % Stop applying the forces after some time 
-    if i==25
+    if i==1
         f =[0;0];
         tau_23N=[0;0];
     end
+    % Recalculate the right hand side, for time dependancy
+    rhs = vertcat([ztwo;Itwo;Itwo]*0.5*tau_23N+V*f,u_1D,u_3D);
 end
 
 
 %% Plotting up the solution
-% u1 = p(1:2,:);
-% u2 = p(3:4,:);
-% u3 = p(5:6,:);
-% X = [u1(1,:);u2(1,:);u3(1,:)];
-% Y = [u1(2,:);u2(2,:);u3(2,:)];
 Px = p(1:2:5,:);
 Py = p(2:2:6,:);
 fig = figure;
-axis([-1.25 1.25 -0.25 1.25])
+axis([-1.25 1.25 -0.25 3])
+grid on
 for i=1:200
     if ~ishandle(fig)
         break
