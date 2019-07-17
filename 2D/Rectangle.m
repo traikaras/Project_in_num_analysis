f6aced4ca7617b09d6fc9099b360662e16d2ca5d
siggisigg <sig.ag.sig@gmail.com>
2019-07-17 13:46:49 +0200

    Made a function around the triangilation

---
+++ 2D/Rectangle.m
@@ -0,0 +1,19 @@
+function [B,etri,C]=Rectangle(L,hfun)
+hfun = +hfun;
+node = [                % list of xy "node" coordinates
+        0, 1                % outer square
+        0, -1
+        L, 1
+        L, -1 
+        ] ;
+    
+    edge = [                % list of "edges" between nodes
+        1, 3                % outer square 
+        3, 4
+        2, 4
+        2, 1 
+        ] ;
+%------------------------------------------- call mesh-gen.
+               % uniform "target" edge-lengths
+   [B,etri,C,tnum] = refine2(node,edge,[],[],hfun) ;
+end
